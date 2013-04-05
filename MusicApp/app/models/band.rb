72701a8c0fd721a873ad523d1db4fc81e641141d
App Academy Student <student@appacademy.io>
2013-04-04 19:14:25 -0700

    reviewed

--- MusicApp/app/models/band.rb
+++ MusicApp/app/models/band.rb
@@ -1,6 +1,6 @@
 class Band < ActiveRecord::Base
   attr_accessible :name
-  has_many :recordings
+  has_many :recordings #REV: not through albums?
   has_many :albums
   has_many :artists, :through => :band_artists
 
